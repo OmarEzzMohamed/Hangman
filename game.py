b1eadd3a9e4b22bae970d2686667e475386f688d
Omar Ezz Mohamed <77416403+OmarEzzMohamed@users.noreply.github.com>
2024-06-06 21:40:57 +0300

    Main Game File

--- game.py
+++ game.py
@@ -3,68 +3,69 @@ import random
 
 def main():
     points = 0
-    print(f'''=============
+    print('''=============
 ---Hangman---
 =============
-Points: {points}
-=============''')
+Points: {}
+============='''.format(points))
+
     # Start of a game
-    while 1:
+    while True:
         # round = word
         y = round(points)
-        if y : # Win Round
-            points +=1
-            print(f'''=============
-Points: {points}
-=============''')
-        else: # Lose Round
-            print(f'''=============
+        if y:  # Win Round
+            points += 1
+            print('''Points: {}
+============='''.format(points))
+        else:  # Lose Round
+            print('''=============
 Game Over
 =============
-Points: {points}
-=============''')
+Points: {}
+============='''.format(points))
             exit()
-    
+
 def round(points):
     word = random.choice(words)
-    word_guess = [*len(word)*"_"]
-    wrong = set() # To show Wrong letters
+    word_guess = ["_" for _ in range(len(word))]
+    wrong = set()  # To show Wrong letters
     lives = -1
+    
     # Start of a guess
-    while 1:
+    while True:
         if lives > -1:
             print(HANGMANPICS[lives])
-        if word == "".join(word_guess): # If user got the word right
-            print(f'''=============
-Good Job!
+            
+        if word == "".join(word_guess):  # If user got the word right
+            print('''Good Job!
 =============''')
             return 1
+        
         print(" ".join(word_guess), end="")
         if wrong:
             print()
-            print(f"Wrong: {', '.join(wrong)}", end="")
+            print("Wrong: {}".format(', '.join(wrong)), end="")
         print("\n=============")
         u_guess = input("Guess a letter: ")
-        if u_guess in word and u_guess not in word_guess: # Letter has been written once
+        
+        if u_guess in word and u_guess not in word_guess:  # Letter has been written once
             print('''=============
 Right Guess!
 =============''')
             for i in range(len(word)):
                 if word[i] == u_guess:
                     word_guess[i] = u_guess
-                
-        else: # Wrong Guess
-            if lives == 5: #Game Over
-                print(f'''==============
-{HANGMANPICS[6]}
-Word: {word}''')
+        else:  # Wrong Guess
+            if lives == 5:  # Game Over
+                print('''==============
+{}
+Word: {}'''.format(HANGMANPICS[6], word))
                 return 0
-            else: # Retry
-                lives +=1
+            else:  # Retry
+                lives += 1
                 wrong.add(u_guess)
-                print(f'''=============
-Wrong Guess! | {6-lives} tries left''')
-            
+                print('''=============
+Wrong Guess! | {} tries left'''.format(6 - lives))
 
 if __name__ == "__main__":
     main()
